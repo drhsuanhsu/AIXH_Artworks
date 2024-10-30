7f63fc1490aa59b33b3e2710aadea105938a9350
Hsuan Hsu 許軒 <dr.hsuanhsu@gmail.com>
2024-10-30 16:17:00 +0800

    Update url.js
    Oct 30, 2024_AI Virtual Influencer Survey

--- url.js
+++ url.js
@@ -1,14 +1,13 @@
 /*
- * 將要隨機派發的網址放入下方（不需排序）
+ * 將要隨機派發的網址放入下方（不需排序） Insert the URLs to be randomly distributed below (no need to sort them)
  *
- * 並請注意：
- * 1. 除最後一項外，其餘項目用逗號分隔
- * 2. 網址請用引號包起來
+ * 請注意： Please note:
+ * 1. 除最後一項外，其餘項目用逗號分隔 Separate each entry with a comma, except for the last item
+ * 2. 網址請用引號包起來 Enclose each URL in quotation marks
  */
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/W3UmRb6Zm1UjgcYc8',
+	'https://forms.gle/oy2Wmuu5tGPjqt5r8'
 ];
